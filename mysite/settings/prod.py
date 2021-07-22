a32104b66bc24e0743bb5062e3f98d61c8bca2a1
yoongyo <jyg0172@naver.com>
2021-07-22 11:45:01 +0900

    Ad

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -27,9 +27,9 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION_STATIC)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_DIRS = os.path.join(BASE_DIR, 'mysite', 'static')
-STATICFILES_DIRS = [
-    STATIC_DIRS
-]
+# STATIC_DIRS = os.path.join(BASE_DIR, 'mysite', 'static')
+# STATICFILES_DIRS = [
+#     STATIC_DIRS
+# ]
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION_MEDIA)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
